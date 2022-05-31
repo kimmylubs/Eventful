2593551b2d394405c3199e16b740866e6760fd44
Kim <wukimberly1@gmail.com>
2022-05-31 19:55:27 -0400

    eventdb

--- server/db/index.js
+++ server/db/index.js
@@ -1,14 +1,19 @@
 //this is the access point for all things database related!
 
-const db = require('./db')
+const db = require("./db");
 
-const User = require('./models/User')
+const User = require("./models/User");
+const Event = require("./models/Event");
 
 //associations could go here!
+
+User.hasMany(Event);
+Event.hasMany(User);
 
 module.exports = {
   db,
   models: {
     User,
+    Event,
   },
-}
+};
