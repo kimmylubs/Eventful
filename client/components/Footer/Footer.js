6ce9583b64acdae7c33562b3eaa4a373665b9c59
Miyuki Nozawa <nozawa.myk@gmail.com>
2022-06-04 23:57:11 -0400

    add navbar, upgrade dependencies & add Layout component

---
+++ client/components/Footer/Footer.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import InstagramIcon from '@mui/icons-material/Instagram';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import TwitterIcon from '@mui/icons-material/Twitter';
+
+import "./Footer.scss";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="footer-container">
+        <span>App Logo</span>
+        <span>Contact us</span>
+        <span>
+          <InstagramIcon />
+          <FacebookIcon />
+          <TwitterIcon />
+        </span>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
