ea973004e80f857034e68663b3c1943fe0f9ca92
Kim <wukimberly1@gmail.com>
2022-06-06 21:31:18 -0400

    loading

--- client/store/events.js
+++ client/store/events.js
@@ -1,21 +1,30 @@
-import axios from 'axios';
+import axios from "axios";
 
-const FETCH_EVENT = 'GET_EVENT';
+const FETCH_EVENT = "FETCH_EVENT";
+const FETCH_CATEGORIES = "FETCH_CATEGORIES";
 
-const _fetchEvents = (events) => (FETCH_EVENT, events)
+const _fetchEvents = (events) => (FETCH_EVENT, events);
+const _fetchCategories = (ategories) => (FETCH_CATEGORIES, categories);
 
 export const getEvents = () => {
   return async (dispatch) => {
-    const events = (await axios.get('/api/events')).data;
+    const events = (await axios.get("/api/events")).data;
     dispatch(_fetchEvents(events));
   };
 };
 
+export const getCategories = () => {
+  return async (dispatch) => {
+    const categories = (await axios.get("/api/categories")).data;
+    dispatch(_fetchCategories(categories));
+  };
+};
+
 const events = (state = [], action) => {
-    if (action.type = FETCH_EVENT){
-        return action.events;
-    };
-    return state;
+  if ((action.type = FETCH_EVENT)) {
+    return action.events;
+  }
+  return state;
 };
 
 export default events;
