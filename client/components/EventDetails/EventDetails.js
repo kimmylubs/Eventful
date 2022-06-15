fbc883ff7f94df5767487833d04af9e23ee154b8
Miyuki Nozawa <nozawa.myk@gmail.com>
2022-06-15 00:16:32 -0400

    display event details

--- client/components/EventDetails/EventDetails.js
+++ client/components/EventDetails/EventDetails.js
@@ -1,78 +1,79 @@
 import React, { Component } from "react";
-import { connect } from "react-redux";
+import { connect, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
 
 import Avatar from "@mui/material/Avatar";
-import AvatarGroup from '@mui/material/AvatarGroup';
-import Paper from '@mui/material/Paper';
+import AvatarGroup from "@mui/material/AvatarGroup";
+import Paper from "@mui/material/Paper";
 
 import { getEvents } from "../../store";
 
 import "./EventDetails.scss";
 
 
-class EventDetails extends Component {
-  state = {
+const parseTime = (time) => {
+  if (!time) return "";
 
-  };
+  const hrs = +time.substring(0, 2);
+  const hh = ((+hrs + 11) % 12) + 1;
+  const mm = time.substring(3, 5);
+  return `${hh}:${mm} ${hrs > 11 ? "PM" : "AM"}`;
+};
+
+class EventDetails extends Component {
 
   render() {
     const { event } = this.props;
+    console.log("event: ", event);
 
     return (
-      <div className="event-details">
-        <div className="event-details-main">
-          <div className="details-left">
-            <img className="event-img"/>
-          </div>
-          <div className="details-right">
-            <div className="event-name">event name</div>
-            <div className="event-date">2022-06-14, 10:00</div>
-            <div className="event-place">Central Park, NY, NY</div>
-            <div className="event-holder">
-              <div className="user-avator">
-                <Avatar sx={{ width:100, height: 100 }}></Avatar>
-              </div>
-              <div className="comments">
-                <Paper 
-                  elevation={3}
-                  sx={{ width:350, height: 200 }}
-                >
-                  comments
-                </Paper>
+      event && (
+        <div className="event-details">
+          <div className="event-details-main">
+            <div className="details-left">
+              <img src={event.imageUrl} className="event-img" />
+            </div>
+            <div className="details-right">
+              <div className="event-name">{event.name}</div>
+              <div className="event-date">{event.date}, {parseTime(event.time)}</div>
+              <div className="event-place">{event.address}, {event.city}</div>
+              <div className="event-holder">
+                <div className="user-avator">
+                  <Avatar sx={{ width: 100, height: 100 }}></Avatar>
+                </div>
+                <div className="comments">
+                  <Paper elevation={3} sx={{ width: 350, height: 200 }}>
+                    comments
+                  </Paper>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-        <div className="event-join-list">
-          <div className="join-list-left">
-            <div className="join-users">who join this event</div>
-            <div className="avatar-container">
-              <AvatarGroup max={7}>
-                <Avatar sx={{ width: 80, height: 80 }}></Avatar>
-                <Avatar sx={{ width: 80, height: 80 }}></Avatar>
-                <Avatar sx={{ width: 80, height: 80 }}></Avatar>
-                <Avatar sx={{ width: 80, height: 80 }}></Avatar>
-                <Avatar sx={{ width: 80, height: 80 }}></Avatar>
-                <Avatar sx={{ width: 80, height: 80 }}></Avatar>
-                <Avatar sx={{ width: 80, height: 80 }}></Avatar>
-                <Avatar sx={{ width: 80, height: 80 }}></Avatar>
-              </AvatarGroup>
+          <div className="event-join-list">
+            <div className="join-list-left">
+              <div className="join-users">who join this event</div>
+              <div className="avatar-container">
+                <AvatarGroup max={7}>
+                  <Avatar sx={{ width: 80, height: 80 }}></Avatar>
+                  <Avatar sx={{ width: 80, height: 80 }}></Avatar>
+                  <Avatar sx={{ width: 80, height: 80 }}></Avatar>
+                  <Avatar sx={{ width: 80, height: 80 }}></Avatar>
+                  <Avatar sx={{ width: 80, height: 80 }}></Avatar>
+                  <Avatar sx={{ width: 80, height: 80 }}></Avatar>
+                  <Avatar sx={{ width: 80, height: 80 }}></Avatar>
+                  <Avatar sx={{ width: 80, height: 80 }}></Avatar>
+                </AvatarGroup>
+              </div>
+            </div>
+            <div className="join-list-right">
+              <span className="join-btn">Join this event</span>
+              <span className="invite-btn">Invite friends</span>
             </div>
           </div>
-          <div className="join-list-right">
-            <span className="join-btn">Join this event</span>
-            <span className="invite-btn">Invite friends</span>
-          </div>
         </div>
-
-      </div>
-    )
-  };
-
-};
-
-const mapStateToProps = (state) => {
-  return state;
+      )
+    );
+  }
 }
 
 const mapDispatchToProps = (dispatch) => {
@@ -81,4 +82,11 @@ const mapDispatchToProps = (dispatch) => {
   };
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(EventDetails);
+const EventDetailsWrapper = () => {
+  const events = useSelector(state => state.events);
+  const { id } = useParams();
+  const event = events.find((event) => event.id === +id);
+  return <EventDetails event={event} />;
+};
+
+export default connect((state) => state, mapDispatchToProps)(EventDetailsWrapper);
--- client/components/UpcomingEvents/UpcomingEvents.js
+++ client/components/UpcomingEvents/UpcomingEvents.js
@@ -15,10 +15,19 @@ import "./UpcomingEvents.scss";
 const UpcomingEvents = (props) => {
   const [prevCounter, setPrevCounter] = useState(0);
   const [nextCounter, setNextCounter] = useState(0);
-  const events = useSelector(state => state.events);
+  const events = useSelector((state) => state.events);
 
   const handlePrev = () => setPrevCounter(prevCounter + 1);
   const handleNext = () => setNextCounter(nextCounter + 1);
+
+  const parseTime = (time) => {
+    if (!time) return "";
+
+    const hrs = +time.substring(0, 2);
+    const hh = ((+hrs + 11) % 12) + 1;
+    const mm = time.substring(3, 5);
+    return `${hh}:${mm} ${hrs > 11 ? "PM" : "AM"}`;
+  };
 
   return (
     <div className="upcoming-events">
@@ -32,8 +41,12 @@ const UpcomingEvents = (props) => {
                 <div className="img"></div>
                 <div className="text">
                   <div className="event-name">{event.name}</div>
-                  <div className="event-date">{event.date}, {event.time}</div>
-                  <div className="event-place">{event.address}, {event.city}</div>
+                  <div className="event-date">
+                    {event.date}, {parseTime(event.time)}
+                  </div>
+                  <div className="event-place">
+                    {event.address}, {event.city}
+                  </div>
                 </div>
               </Link>
             </SwiperSlide>
