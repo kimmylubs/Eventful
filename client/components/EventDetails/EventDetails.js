7a95573185bdf9c6dcedf6c882cdcf86f72f385f
Miyuki Nozawa <nozawa.myk@gmail.com>
2022-06-20 18:58:00 -0400

    fix styling of event details page

--- client/components/EventDetails/EventDetails.js
+++ client/components/EventDetails/EventDetails.js
@@ -4,7 +4,6 @@ import { useParams } from "react-router-dom";
 
 import Avatar from "@mui/material/Avatar";
 import AvatarGroup from "@mui/material/AvatarGroup";
-import Paper from "@mui/material/Paper";
 
 import { joinOrLeaveEvent, selectUser, selectEvent } from "../../store";
 import { parseTime, parseDate, getDayOfWeek, getHasUserJoinedEvent } from "../../utils";
@@ -26,56 +25,48 @@ const EventDetails = (props) => {
   return (
     event && (
       <div className="event-details">
-        <div className="event-details-main">
-          <div className="details-left">
+        <div className="details-left">
+          <div className="image-container">
             <img src={event.logo} className="event-img" />
-            <div className="event-place">
-              View on <a href={event.url}>EventBrite</a>
-            </div>
+            <span>View on <a href={event.url}>EventBrite</a></span>
           </div>
-          <div className="details-right">
-            <div className="event-name">{event.name}</div>
-            <div className="event-date">
-              {getDayOfWeek(event.localStart)}, {parseDate(event.localStart)},{" "}
-              {parseTime(event.localStart)}
-            </div>
-            <div className="event-place">
-              <div> {event.venueName}</div> {event.localizedAddress}
-            </div>
-            <div className="event-holder">
-              <div className="user-avator">
-                <Avatar sx={{ width: 100, height: 100 }}></Avatar>
-              </div>
-              <div className="comments">
-                <Paper elevation={3} sx={{ width: 260, height: 140, padding: 3 }}>
-                  <div>
-                    {" "}
-                    <b>Event Description </b>
-                    <br></br>
-                    {event.description}{" "}
-                  </div>
-                </Paper>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="event-join-list">
-          <div className="join-list-left">
+          <div className="event-join-list">
             <div className="join-users">who join this event</div>
             <div className="avatar-container">
-              <AvatarGroup max={7}>
-                <Avatar sx={{ width: 80, height: 80 }}></Avatar>
-                <Avatar sx={{ width: 80, height: 80 }}></Avatar>
-                <Avatar sx={{ width: 80, height: 80 }}></Avatar>
-                <Avatar sx={{ width: 80, height: 80 }}></Avatar>
-                <Avatar sx={{ width: 80, height: 80 }}></Avatar>
-                <Avatar sx={{ width: 80, height: 80 }}></Avatar>
-                <Avatar sx={{ width: 80, height: 80 }}></Avatar>
-                <Avatar sx={{ width: 80, height: 80 }}></Avatar>
+              <AvatarGroup max={6} className="avatars">
+                <Avatar className="avatar"></Avatar>
+                <Avatar className="avatar"></Avatar>
+                <Avatar className="avatar"></Avatar>
+                <Avatar className="avatar"></Avatar>
+                <Avatar className="avatar"></Avatar>
+                <Avatar className="avatar"></Avatar>
+                <Avatar className="avatar"></Avatar>
+                <Avatar className="avatar"></Avatar>
               </AvatarGroup>
             </div>
           </div>
-          <div className="join-list-right">
+        </div>
+        <div className="details-right">
+          <div className="event-name">{event.name}</div>
+          <div className="event-date">
+            {getDayOfWeek(event.localStart)}, {parseDate(event.localStart)},{" "}
+            {parseTime(event.localStart)}
+          </div>
+          <div className="event-place">
+            <div> {event.venueName}</div> 
+            <div>{event.localizedAddress}</div>
+          </div>
+          <div className="event-holder">
+            <div className="event-owner">
+              <Avatar className="avatar"></Avatar>
+            </div>
+            <div className="comments">
+              <div className="speech-bubble">
+                <div>{event.description}</div>
+              </div>
+            </div>
+          </div>
+          <div className='btn-container'>
             <span
               className={`join-btn ${hasJoinedEvent ? "joined" : ""}`}
               onClick={handleJoinOrLeave}
